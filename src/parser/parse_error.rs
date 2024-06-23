c58573f9e9902bf9d0cdb81f96b80b80293344c0
Irjan Vuckovic Olsen <irjan.olsen@gmail.com>
2024-06-23 13:41:00 +0200

    Moved the section parsing to the section.rs file

--- src/parser/parse_error.rs
+++ src/parser/parse_error.rs
@@ -1,7 +1,10 @@
+use std::io;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum McaParseError {
+    #[error("Error loading world: {0}")]
+    WorldLoadError(#[from] io::Error),
     #[error("Faled parsing NBT: {0}")]
     NbtParseError(#[from] inbt::NbtParseError),
     #[error("Hit end of data")]
--- src/parser/region.rs
+++ src/parser/region.rs
@@ -129,62 +129,9 @@ impl Region {
     pub fn parse_sections(data: Vec<NbtTag>) -> Result<Vec<Section>, McaParseError> {
         let mut sections = vec![];
         for tag in data {
-            sections.push(Self::parse_section(tag)?)
+            sections.push(Section::parse_section(tag)?)
         }
         Ok(sections)
-    }
-
-    pub fn parse_section(tag: NbtTag) -> Result<Section, McaParseError> {
-        let block_states = tag.get("block_states")?;
-        let palette = block_states.get_list("palette")?;
-        if palette.len() == 1 {
-            return Ok(Section::new(vec![palette[0].get_string("Name")?; 4096], tag));
-        }
-        let block_data = block_states.get_long_array("data")?;
-
-        // Bits needed to store the index into palette list, minimum 4 bits.
-        let mut palette_bits = palette.len().checked_ilog2().unwrap_or(0) as usize;
-        if usize::pow(2, palette_bits as u32) < palette.len() {
-            palette_bits += 1;
-        }
-        if palette_bits < 4 {
-            palette_bits = 4;
-        }
-        // Calculate the palette mask
-        let mut palette_mask = 0b0;
-        for _ in 0..palette_bits {
-            // There's probably a better way of doing this..
-            palette_mask <<= 1;
-            palette_mask |= 1;
-        }
-        let palette_entries_per_long = 64/palette_bits;
-        let padding = 64%palette_bits;
-
-        let mut blocks = vec![];
-        for y in 0..16 {
-            for z in 0..16 {
-                for x in 0..16 {
-                    let block_pos = y*16*16 + z*16 + x;
-                    let block_data_index = block_pos/palette_entries_per_long;
-                    let block_data_sub_index = block_pos%palette_entries_per_long + 1;
-                    let mask_shift = (64-padding)-palette_bits*block_data_sub_index;
-                    let palette_index = (block_data[block_data_index] as u64 & (palette_mask<<mask_shift))>>mask_shift;
-                    if palette_index as usize >= palette.len() {
-                        // Will panic after this, just debug info for now
-                        eprintln!("palette_bits: {}", palette_bits);
-                        eprintln!("palette_mask: {:0b}", palette_mask);
-                        eprintln!("block_data: {:064b}", block_data[block_data_index]);
-                        eprintln!("block_data_index: {}", block_data_sub_index-1);
-                        eprintln!("palette_index: {palette_index}");
-                    }
-                    let block = &palette[palette_index as usize];
-                    let block_name = block.get_string("Name").unwrap();
-                    blocks.push(block_name);
-                }
-            }
-        }
-
-        Ok(Section::new(blocks, tag))
     }
 
     pub fn parse_region(region_data: Vec<u8>) -> Result<Region, McaParseError> {
